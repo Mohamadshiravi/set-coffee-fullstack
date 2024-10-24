09d530316f05e1beed436c006d145485e134534a
mohamad <mohamadshiravi85@gmail.com>
2024-10-24 16:39:29 +0330

    connect-to-cloud-db

--- utils/connecttodb.js
+++ utils/connecttodb.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 export default async function ConnectTODb() {
+  const uri =
+    "mongodb+srv://mohamadshiravi85:MTshirav1@cluster.4iwnc.mongodb.net/?retryWrites=true&w=majority&appName=setcoffee";
   try {
     if (mongoose.connections[0].readyState) {
       return false;
     }
-    await mongoose.connect("mongodb://localhost:27017/set-coffee");
+    await mongoose.connect(uri);
   } catch (error) {}
 }
